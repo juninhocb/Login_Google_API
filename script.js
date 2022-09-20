dd8adb1a2bbe9d0a93b9c76384bae79dae203663
juninhocb <juninhocb@hotmail.com>
2022-09-19 21:40:41 -0300

    prod

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function handleCredentialResponse(response) {
+    const data = jwt_decode(response.credential)
+    console.log(data)
+    if (data.email_verified === true){
+        fullName.textContent = "Bem vindo " + data.name
+        document.getElementById("mImg").style.display = "none"; 
+        document.getElementById("buttonDiv").style.display = "none";
+        document.getElementById("mPar").innerHTML = "&#128512;";
+        document.getElementById("mTitle").innerHTML = "Logado com scuesso!!!" 
+        document.getElementById("mImgShow").setAttribute('src', data.picture); 
+        
+    } else {
+
+        fullName.textContent = "Falha ao autenticar-ses"
+    }
+
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+    client_id: "<your_client_id_here>",
+    callback: handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { 
+        theme: "outline", 
+        size: "large",
+        type:"standard",
+        shape: "pill"
+    } 
+    );
+
+    google.accounts.id.prompt(); 
+}
